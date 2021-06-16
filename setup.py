3810265f1de766abf011356318ca067c1513d9f4
Orson Oehlers <orson@oehlers.net>
2021-06-16 11:55:34 -0400

    bump to v0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="arubabank",
-    version="0.0.1",
+    version="0.1.0",
     description="Aruba Bank API",
     long_description=long_description,
     long_description_content_type="text/markdown",
