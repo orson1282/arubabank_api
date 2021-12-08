fe052dc3bf00b8e9dde54d55cfc7f0a631065153
Orson Oehlers <orson@oehlers.net>
2021-12-07 23:49:23 -0400

    - bump to v0.2.0 - added some logic to handle failed responses - added 120s timeout for refresh to keep session active - added get_portfolio_ids to api class - cleaned setup.py - updated readme - updated .gitignore - removed global vars and added them into functions - added function for parsing cli arguments

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 PACKAGE_NAME = "arubabankapi"
 AUTHOR = "Orson Oehlers"
 AUTHOR_EMAIL = "orson@oehlers.net"
